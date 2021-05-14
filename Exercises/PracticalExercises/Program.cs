cd012ca9c2ac0001504c8a94b9877877cccdc85b
DARIEM MACIAS MORA <335286@dadeschools.net>
2021-05-14 09:40:12 -0400

    COMMIT

---
+++ Exercises/PracticalExercises/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ExercisesLearning
+{
+  class Program
+  {
+
+    public static void Main(string[] args)
+    {
+
+      var numericalMethods = new BasicAlgorithms.NumericalMethodsRepository();
+      var stringMethods = new BasicAlgorithms.StringsMethodsRepository();
+
+          /*  95.
+      BasicAlgorithms.NumericalMethodsRepository.newArrayFromDictionary(new Dictionary<int, List<int>>(){
+        {3,new List<int>{2,3,5,3,4}},
+        {1,new List<int> {2,4,3}}
+        }
+        ); 
+          
+        var newDictionary= new Dictionary<int, List<int>>();
+        newDictionary.Add(1, new List<int> {10,20,34});
+        newDictionary.Add(2, new List<int>{10,20,30});
+        var arraylargestSum = BasicAlgorithms.NumericalMethodsRepository.ComputeSumList(newDictionary);
+
+        foreach (var item in arraylargestSum)
+	{
+        Console.Write(item + " ");
+
+            Testing new update.
+	}          
+           */
+         
+/*     var List = numericalMethods.ArrayWithNoMultipleOfValue(new List<int>{ 10, 22, 35 , 47, 53, 67 }, 7);
+
+        foreach (var item in List)
+	{
+                Console.Write(item + " ");
+	}
+  */        
+
+     //Console.WriteLine(List);
+
+            numericalMethods.DisplayFunctionValue();
+    }
+  
+  }
+
+}
+
+
