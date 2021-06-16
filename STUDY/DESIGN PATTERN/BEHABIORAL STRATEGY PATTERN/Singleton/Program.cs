c39f5014c95b2e219cffb643df98a52ab5040f2d
Dariem Macias Mora <dariem.carlos@hotmail.com>
2021-06-16 17:54:33 -0400

    Update project

--- STUDY/DESIGN PATTERN/BEHABIORAL STRATEGY PATTERN/Singleton/Program.cs
+++ STUDY/DESIGN PATTERN/BEHABIORAL STRATEGY PATTERN/Singleton/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Singleton.Singleton;
 using System;
+using System.Threading.Tasks;
 
 namespace Singleton
 {
@@ -8,12 +9,29 @@ namespace Singleton
 
         static void Main(string[] args)
         {
+            /*
+            For example Singleton1 and Singleton2
 
-            var fromTeacher = Singleton1.GetInstance;
-            var fromStudent = Singleton1.GetInstance;
+            var fromTeacher = Singleton2.GetInstance;
+            var fromStudent = Singleton2.GetInstance;
 
             fromTeacher.PrintMessage("From Teacher");
             fromStudent.PrintMessage("From Student");
+
+            
+            //Instantiating Singleton class from derived class.
+            //This violates singleton pattern principle.
+            
+
+            Singleton2.DerivedSingleton derivedObject = new Singleton2.DerivedSingleton();
+            derivedObject.PrintMessage("From derived");
+            */
+
+            Parallel.EventHandler temp = MyEvent;
+            if (temp != null)
+            {
+                temp();
+            }
 
             Console.ReadKey();
         }
--- STUDY/DESIGN PATTERN/BEHABIORAL STRATEGY PATTERN/Singleton/Singleton/Singleton1.cs
+++ STUDY/DESIGN PATTERN/BEHABIORAL STRATEGY PATTERN/Singleton/Singleton/Singleton1.cs
@@ -5,12 +5,13 @@ using System.Text;
 namespace Singleton.Singleton
 {
 
-    /*In sigleton Design Pattern a class has only one instance in the program, that provides a global point of access to it.
-    Is a class that allow only a single instance of itself to be created and ussually gives simple access to the instance.
+    /*In Singleton Design Pattern a class has only one instance in the program, that provides a global point of access to it.
+    Is a class that allow only a single instance of itself to be created and usually gives simple access to the instance.
+    We need to use the Singleton design pattern in C# when we need to ensure that only one instance of a particular class is available at any given point of time for the entire application.
 
      There is different ways to implement Singleton pattern. The following are the common characteristics:
 
-    -Private and Parameterless single construcctor.
+    -Private and Parameterless single constructor.
     -Sealed Class.
     -Static variable to hold a reference to the single created instance.
     -A public and static way of getting the reference to the created instance.
@@ -22,16 +23,29 @@ namespace Singleton.Singleton
     - It helps to hide dependencies*.
     - It provides a single point of access to a particular instance, so it easy to maintain.
 
-    Disavantege:
+    Disadvantage:
 
-    - Unity test is dificult as it intruduces a global state into the application.
-    - Reduce the potencial for parallelism within a program by locking.
+    - Unity test is difficult as it introduces a global state into the application.
+    - Reduce the potential for parallelism within a program by locking.
 
     The following compares Singleton class vs. Static methods:
 
     - A Static Class cannot be extended whereas a singleton class can be extended.
     - A Static Class cannot be initialized whereas a singleton class can be.
     - A Static class is loaded automatically by the CLR when the program containing the class is loaded.
+
+    Some Real-time scenarios where you can use the Singleton Design Pattern:
+
+    Service Proxies: As we know invoking a Service API is an extensive operation in an application. The process that taking most of the time is creating the Service client in order to invoke the service API. If you create the Service proxy as Singleton then it will improve the performance of your application.
+
+    Facades: You can also create Database connections as Singleton which can improve the performance of the application.
+
+    Logs: In an application, performing the I/O operation on a file is an expensive operation. If you create your Logger as Singleton then it will improve the performance of the I/O operation.
+
+    Data sharing: If you have any constant values or configuration values then you can keep these values in Singleton So that these can be read by other components of the application.
+
+    Caching: As we know fetching the data from a database is a time-consuming process. In your application, you can cache the master and configuration in memory which will avoid the DB calls. 
+    In such situations, the Singleton class can be used to handle the caching with thread synchronization in an efficient manner which drastically improves the performance of the application. 
     
     -How to Implement Singleton Pattern in C# code?
 
@@ -40,6 +54,25 @@ namespace Singleton.Singleton
     public sealed class Singleton1
     {
         private static int counter = 0;
+        /*
+        private static variable that hold reference to the single created instance of the class if any.
+        Initialized it to null to ensure that one instance of the class is created based on the null condition.
+        */
+        private static Singleton1 instance = null;
+        /*
+        public static property/Method which will return the single-created instance (only one instance) of the singleton class by checking the value of the private variable instance. 
+        This method or property first check if an instance of the singleton class is available or not. If the singleton instance is available, then it returns that singleton instance otherwise it will create an instance and then return that instance.
+        */
+        public static Singleton1 GetInstance {
+
+            get {
+
+               return instance ?? new Singleton1();
+               
+            }
+
+        }
+
 
         //Constructor private and parameterless.This is required because it is not allowed the class to be instantiated from outside the class. It only instantiates from within the class.
         private Singleton1(){
@@ -47,24 +80,6 @@ namespace Singleton.Singleton
             Console.WriteLine("Object instance Counter value: {0}", counter.ToString());
         }
         
-        //private static variable that hold reference to the single created instance of the class if any.
-        //Initialized it to null to ensure that one instance of the class is created based on the null condition.
-        private static Singleton1 instance = null;
-
-        //public static property/Method which will return the single-created instance (only one instance) of the singleton class by checking the value of the private variable instance. 
-        //This method or property first check if an instance of the singleton class is available or not. If the singleton instance is available, then it returns that singleton instance otherwise it will create an instance and then return that instance.
-        public static Singleton1 GetInstance {
-
-            get {
-
-                if (instance == null) {
-                    instance = new Singleton1();
-                }
-                return instance;
-            }
-
-        }
-
         public void PrintMessage(string message) {
 
             Console.WriteLine(message);
