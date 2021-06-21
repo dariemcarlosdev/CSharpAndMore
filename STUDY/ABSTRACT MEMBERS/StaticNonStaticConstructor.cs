e5ffe7ed08656d7450bc1bd31cf07d9c491ba1fa
Dariem Macias Mora <dariem.carlos@hotmail.com>
2021-06-21 18:29:33 -0400

    Commit Comment on StaticNonStaticConstructor class example

--- STUDY/ABSTRACT MEMBERS/StaticNonStaticConstructor.cs
+++ STUDY/ABSTRACT MEMBERS/StaticNonStaticConstructor.cs
@@ -4,14 +4,14 @@ namespace ABSTRACT_MEMBERS
 {
     public class StaticNonStaticConstructor
     {
-        
-        /*
-        the static constructor is the fast block of code that gets executes under a class.
-        No matter how many numbers of objects you created for the class the static constructor is executed only once.
+
+        /*        
         non-static constructor gets executed only when we created the object of the class and that is too for each and every object of the class.
         It is not possible to create a static constructor with parameters. This is because the static constructor is the first block of code that is going to execute under a class. 
         This static constructor called implicitly, even if parameterized there is no chance of sending the parameter values.
         */
+
+        //the static constructor is the fast block of code that gets executes under a class. No matter how many numbers of objects you created for the class the static constructor is executed only once.
         static StaticNonStaticConstructor()
         {
             Console.WriteLine("The static constructor is called.");
