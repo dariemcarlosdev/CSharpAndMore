19f1972531c2100f4cc2e76c834d4c9487f10b55
DARIEM MACIAS MORA <335286@dadeschools.net>
2022-03-25 15:43:08 -0400

    Array And ArrayList differences

---
+++ STUDY/ARRAY AND ARRAYLIST/ArrayAndArrayList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Difference between array and arrayList
+
+            //When we want to store item of the same type we use array.
+
+            string[] strArray = new string[] {"a", "b", "c" };
+
+            //When we need to store item with diferent datatypes, we use ArrayList.
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(3);
+            arrayList.Add("Dariem");
+            arrayList.Add(false);
+        }
+    }
+}
