b10557c993014cb4257b9db693d13d9c61d5f540
DARIEM MACIAS MORA <335286@dadeschools.net>
2022-03-26 17:37:44 -0400

    Generics

---
+++ STUDY/GENERICS/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    //Creating a Generic Data Type.
+    public class GFT<T>
+    {
+        public T value { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of string type.
+            GFT<string> Company = new GFT<string>();
+            Company.value = "Company C.0";
+
+            //Creating an instance of float type. Here we use var.
+            var version = new GFT<float>();
+            version.value = 6.0f;
+
+
+
+            Console.WriteLine(Company.value + version.value);
+        }
+
+        
+    }
+}
---
+++ STUDY/SERIALIZATION/SERIALIZE TO JSON/SerializeToJson/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SerializeToJson
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string fileName = "WeatherForecast.json";
+            using (FileStream createStream = File.Create(fileName))
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(createStream, weatherForecast);
+                
+            }
+
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+    }
+}
