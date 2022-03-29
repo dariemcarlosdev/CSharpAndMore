393a8e11260101fdb59a33a36650e4021e70edd6
DARIEM MACIAS MORA <335286@dadeschools.net>
2022-03-29 09:35:39 -0400

    ReversingArray

---
+++ STUDY/ALGORITHM/REVERSING STRING/ReversingArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Reverse.ReverseString("sdvfffvf");
+            Console.WriteLine(a);
+        }
+    }
+
+    public static class Reverse
+    {
+
+        public static string ReverseString(string S)
+        {
+            var ListChar = S.ToCharArray();
+            Array.Reverse(ListChar);
+
+            return new string(ListChar);
+        }
+    
+    }
+}
